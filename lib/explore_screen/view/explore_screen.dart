48cf7db960b6df1eb1fd0cc03a62e58d415ce767
Salih T <mhdsalih9656@gmail.com>
2022-09-12 16:47:58 +0530

    explore_screen_completed

--- lib/explore_screen/view/explore_screen.dart
+++ lib/explore_screen/view/explore_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
