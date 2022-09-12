357b62ceb18022dae9c219e04e3c036311181217
Salih T <mhdsalih9656@gmail.com>
2022-09-12 16:42:23 +0530

    user_controller_completed

--- lib/home_screen/controllers/top_users_controller.dart
+++ lib/home_screen/controllers/top_users_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:grocery_app/home_screen/models/top_users_model.dart';
 
+
 class TopUsersController extends GetxController {
   List<TopUsersModel> userList = [
     TopUsersModel(usersImage: "assets/image.png"),
